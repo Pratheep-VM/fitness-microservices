b8c231e51b9ba8d2d92ae357921c7ee6847779b1
Pratheep-VM <upiratheep24@gmail.com>
2026-01-11 17:54:16 +0530

    fitness-microservices

---
+++ userservice/userservice/src/main/java/com/fitness/userservice/service/UserService.java
@@ -0,0 +1,60 @@
+package com.fitness.userservice.service;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.model.User;
+import com.fitness.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public UserResponse register(RegisterRequest request) {
+
+        if (repository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("Email already exists");
+        }
+
+
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+
+        User userSaved = repository.save(user);
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(userSaved.getId());
+        userResponse.setEmail(userSaved.getEmail());
+        userResponse.setPassword(userSaved.getPassword());
+        userResponse.setFirstName(userSaved.getFirstName());
+        userResponse.setLastName(userSaved.getLastName());
+        userResponse.setCreatedAt(userSaved.getCreatedAt());
+        userResponse.setUpdatedAt(userSaved.getUpdatedAt());
+
+        return userResponse;
+
+
+    }
+
+    public UserResponse getUserProfile(String userId){
+        User user = repository.findById(userId).
+                orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+
+        return userResponse;
+    }
+}
