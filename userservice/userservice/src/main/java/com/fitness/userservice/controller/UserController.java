4bed27866e0f0c4e6938f9460efbff2a18ffb71a
Pratheep-VM <upiratheep24@gmail.com>
2026-01-11 19:03:58 +0530

    user-microservice created

--- userservice/userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 
     }
 
-    @GetMapping("register")
+    @PostMapping("register")
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(userService.register(request));
     }
