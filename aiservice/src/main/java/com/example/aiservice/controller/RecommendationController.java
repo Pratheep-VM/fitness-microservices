428e2b70f0ff4d7ce8b38da78d8723e7ecd32031
Pratheep-VM <upiratheep24@gmail.com>
2026-01-31 01:48:46 +0530

    setup RabbitMQ and fix connection configuration

--- aiservice/src/main/java/com/example/aiservice/controller/RecommendationController.java
+++ aiservice/src/main/java/com/example/aiservice/controller/RecommendationController.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/recommendations")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
@@ -19,7 +21,7 @@ public class RecommendationController {
     public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId){
         return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
     }
-    @GetMapping("/user/{activityId}")
+    @GetMapping("/activity/{activityId}")
     public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId){
         return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
     }
